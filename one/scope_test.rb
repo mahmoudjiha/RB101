b65a235fc8023080ad41a8b8c70fd3cfedee2eaa
mahmoudjiha <107807116+mahmoudjiha@users.noreply.github.com>
2022-10-16 14:39:31 -0700

    10/12

---
+++ one/scope_test.rb
@@ -0,0 +1,11 @@
+# scope_test.rb
+
+NUMBERS = [6, 9, 8]
+array = [99, 12, 104]
+
+def access_test
+  puts NUMBERS.first
+  puts array.last
+end
+
+access_test
---
+++ two/calculator.rb
@@ -0,0 +1,18 @@
+# calculator.rb
+#
+# ask for two numbers
+# ask for an operation to perform
+# perform the operation
+# output the result
+
+puts "Welcome to Calculator!"
+
+puts "What's the first number?"
+number_one = gets.chomp
+
+# puts "The first number is #{number}!"
+
+puts "What's the second number?"
+number_two = gets.chomp
+
+puts "What operation would you like to perform?
