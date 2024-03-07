e07ff4297e75b7d854484d951cb6b4660e9dc890
mahmoudjiha <107807116+mahmoudjiha@users.noreply.github.com>
2024-03-07 06:42:11 -0800

    updating files

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -4,43 +4,108 @@
 ##  the screen.
 
 def prompt(string)
-    puts '>>> ' + string
+  puts ">>> #{string}"
 end
+
+def valid_name?(string)
+  string != '0' && string.to_i.to_s == '0'
+end
+
+def valid_number?(string)
+  string == '0' || string.to_i != 0
+end
+
+def valid_operator?(string)
+  %w(1 2 3 4).include?(string)
+end
+
+ERROR_MESSAGE = "That doesn't look like a valid input...try again"
 
 TERMS = {
-    1 => ['add', 'to'],
-    2 => ['subtract', 'and'],
-    3 => ['multiply', 'with'],
-    4 => ['divid', 'by']
+  '1' => ['add', 'to'],
+  '2' => ['subtract', 'and'],
+  '3' => ['multiply', 'with'],
+  '4' => ['divid', 'by']
 }
 
-prompt('What is the first number?')
-first_number = gets.chomp.to_f
+OPERATIONS = <<-STR
+      1. Addition
+      2. Subtraction
+      3. Multiplication
+      4. Division
+STR
 
-prompt('What is the second number?')
-second_number = gets.chomp.to_f
+prompt('Welcome to Calculator!')
+name = ''
 
-prompt('What operation would you like to perform?')
-print "1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\nEnter an integer 1-4: "
-operator = gets.chomp.to_i
+loop do
+  prompt('What is your name?')
+  name = gets.chomp
 
-result = case operator
-         when 1
-            first_number + second_number
-         when 2
-            first_number - second_number
-         when 3
-            first_number * second_number
-         when 4
-            if second_number == 0
-                'Zero division error!'
-            else
-                first_number / second_number
-            end
-        end
-
-if result.is_a?(String)
-    puts result
-else
-    puts "The result of #{TERMS[operator].first}ing #{first_number} #{TERMS[operator].last} #{second_number} is:\n#{result.round(2)}"
+  break if valid_name?(name)
+  prompt(ERROR_MESSAGE)
 end
+
+prompt("Alright #{name}, let's do this!")
+
+loop do
+  first_number = ''
+  second_number = ''
+  operator = ''
+
+  loop do
+    prompt('What is the first number?')
+    first_number = gets.chomp
+
+    break if valid_number?(first_number)
+    prompt(ERROR_MESSAGE)
+  end
+
+  loop do
+    prompt('What is the second number?')
+    second_number = gets.chomp
+
+    break if valid_number?(second_number)
+    prompt(ERROR_MESSAGE)
+  end
+
+  prompt("What operation would you like to perform?\n#{OPERATIONS}")
+
+  loop do
+    prompt('Enter an integer 1-4: ')
+    operator = gets.chomp
+
+    break if valid_operator?(operator)
+    prompt(ERROR_MESSAGE)
+  end
+
+  result = case operator
+           when '1'
+             first_number.to_i + second_number.to_i
+           when '2'
+             first_number.to_i - second_number.to_i
+           when '3'
+             first_number.to_i * second_number.to_i
+           when '4'
+             if second_number == '0'
+               'Zero division error!'
+             else
+               first_number.to_f / second_number.to_f
+             end
+           end
+
+  if result.is_a?(String)
+    prompt(result)
+  else
+    prompt("The result of #{TERMS[operator].first}ing #{first_number} " \
+           "#{TERMS[operator].last} #{second_number} is: #{result.round(2)}")
+  end
+
+  prompt('Perform another calculation?')
+  print('(Y/n): ')
+  user_input = gets.chomp
+
+  break if user_input[0].upcase != 'Y'
+end
+
+prompt("Thank you for using Calculator, #{name}!")
