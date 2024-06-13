7299abcbfc1fa862908871c3a88b19ba55c1c4d6
mahmoudjiha <107807116+mahmoudjiha@users.noreply.github.com>
2024-06-12 20:38:33 -0700

    refactored

--- exercises/after_midnight.rb
+++ exercises/after_midnight.rb
@@ -1,31 +1,33 @@
 # after_midnight.rb
 
+MINUTES_IN_A_DAY = 1440
+NEGATIVE_HOUR_START = 23
+ZERO = 0
+NEGATIVE_MINUTE_START = 60
+
 def time_of_day(minutes_from_midnight)
-  # divided by 60
-  # add / subtract hour
-  # add / subtract minute
-  # convert to string & format
+  time_differential = minutes_from_midnight.abs % MINUTES_IN_A_DAY
+  hours, minutes = time_differential.divmod(60)
 
-  return '00:00' if minutes_from_midnight == 0
-
-  hours = (minutes_from_midnight / 60).abs.remainder(24)
-
-  minute_from_hour = minutes_from_midnight.remainder(60)
-
-  starting_minute, starting_hour = minute_from_hour > 0 ? [0, 0] : [-60, -24]
-
-  components = [(starting_hour + hours), (starting_minute - minute_from_hour)]
-  components.map! { |element| element.abs.to_s }
-  p components
+  result = case minutes_from_midnight <=> 0
+           when -1
+            [(NEGATIVE_HOUR_START - hours), (NEGATIVE_MINUTE_START - minutes)]
+           when 1
+            [hours, minutes]
+           else
+            [0, 0]
+           end
   
-  components.each_with_index do |element, index|
-    components[index].prepend('0') if element.size == 1
+  result.each_with_index do |element, index|
+    if element < 10
+      result[index]="0#{element}"
+    end
   end
 
-  p components.insert(1, ':').join
+  result.insert(1, ':').join
 end
 
-puts time_of_day(0) == "00:00"
+# puts time_of_day(0) == "00:00"
 puts time_of_day(-3) == "23:57"
 puts time_of_day(35) == "00:35"
 puts time_of_day(-1437) == "00:03"
