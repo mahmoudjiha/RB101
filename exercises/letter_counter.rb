321a824af9305544a78c4f12bc972eba1123e01c
mahmoudjiha <107807116+mahmoudjiha@users.noreply.github.com>
2024-06-17 16:35:25 -0700

    refactoring

--- exercises/letter_counter.rb
+++ exercises/letter_counter.rb
@@ -3,26 +3,15 @@
 ALPHABET = /[a-zA-Z]/
 
 def word_sizes(string)
-  words = string.split
+  words = string.split.map! { |word| word.chars.grep(ALPHABET).join }
 
-  word_lengths = words.map do |word|
-    word.chars.select { |character| ALPHABET.match?(character) }.join.size
-  end
-
-  sizes = word_lengths.uniq
-  result = {}
-
-  if !sizes.empty?
-    sizes.each do |size|
-      count = word_lengths.select { |word_length| word_length == size }
-      result[size] = count.length
-    end
-  end
-
+  result = Hash.new(0)
+  words.each { |word| result[word.size] += 1 }
   result
 end
 
 puts word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
-puts word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+puts word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5,
+                                                                   6 => 3 }
 puts word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
 puts word_sizes('') == {}
